a6b89ff9669b3f99d1f1d98eb37b073f21e99d92
amanjitkaur <38258063+amanjitkaur@users.noreply.github.com>
2019-06-01 10:22:43 +0530

    Added spinner for mobile number's country code.Added a dependency for spinner layout. "implementation 'com.github.ganfra:material-spinner:2.0.0' "

--- app/src/main/java/com/example/rentpe/SignupPage.java
+++ app/src/main/java/com/example/rentpe/SignupPage.java
@@ -2,12 +2,28 @@ package com.example.rentpe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import fr.ganfra.materialspinner.MaterialSpinner;
 
 public class SignupPage extends AppCompatActivity {
+    private static final String[] ITEMS = {"+91", "+44", "+62","+44 1624","+39 06 698","+61 89164"};
 
+    private ArrayAdapter<String> adapter;
+
+    MaterialSpinner spinner6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_page);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, ITEMS);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        initSpinnerHintAndCustomHintView();
+
+    }
+
+    private void initSpinnerHintAndCustomHintView() {
+        spinner6 = findViewById(R.id.spinner6);
+        spinner6.setAdapter(adapter);
     }
 }
